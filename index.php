e9398e0e67b52591cd65294106ac88afdab92863
Will Hawks <will@creativedistillery.com>
2019-10-04 22:51:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php include('env.php'); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <title>Your Vote Matters! | Vote for Change on November 5th</title>
+
+    <meta name="description" content="On November 5th you will have the opportunity to elect a new Governor, Lt. Governor and Legislature. You can be the vote that decides our state's path by getting out and voting for Mississippi's next leaders." />
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="dist/favicon.png" />
+
+    <link rel="stylesheet" href="https://use.typekit.net/egt1epc.css">
+    <link rel="stylesheet" href="dist/styles<?php echo $env == 'production' ? '.min' : ''; ?>.css">
+
+  </head>
+
+  <body>
+
+    <header class="header">
+      <div class="header__logo-wrapper">
+        <img class="header__logo" src="dist/your-vote-matters.png" alt="Your vote matters!" />
+      </div>
+      <div class="header__side">
+        <div class="header__side-img-wrapper">
+          <img class="header__side-img" src="dist/header-side-img.jpeg" alt="Portrait of a Medgar Evers." />
+        </div>
+        <div class="header__side-img-wrapper">
+          <img class="header__side-img" src="dist/Fannie_Lou_Hamer_1964-08-22.jpg" alt="Fannie Lou Hamer, 1964." />
+        </div>
+        <div class="header__side-img-wrapper">
+          <img class="header__side-img" src="dist/elliott-stallion-1UY8UuUkids-unsplash.jpg" alt="Exterior door with a sign labeled 'Polling Station.'" />
+        </div>
+        <h3 class="header__side-heading">
+          Vote for change on November 5<span>th</span>
+        </h3>
+      </div>
+    </header>
+
+    <div class="content">
+      <p>Mississippi heroes like Fannie Lou Hamer and Medgar Evers fought, bled and died because they knew YOUR VOTE MATTERS! Today, your matters more than ever.</p>
+      <p>On November 5th you will have the opportunity to elect a new Governor, Lt. Governor and Legislature. <strong>You can be the vote</strong> that decicdes ours state's path by getting out and voting for Mississippi's next leaders.</p>
+    </div>
+
+    <div class="cards">
+      <div class="card">
+        <img class="card__image" src="dist/student.jpg" alt="Students" />
+        <h3 class="card__heading">Your public schools are on the ballot!</h3>
+        <p class="card__text">You can be the vote that ensures your tax dollars go to public schools and not private academies.</p>
+      </div>
+      <div class="card">
+        <img class="card__image" src="dist/healthcare.jpg" alt="Doctor talking to woman on hospital bed and man beside her." />
+        <h3 class="card__heading">Your health care is on the ballot!</h3>
+        <p class="card__text">You can be the vote that expands Medicaid and gives health care to thousands of Mississippians.</p>
+      </div>
+      <div class="card">
+        <img class="card__image" src="dist/road.jpg" alt="Stop sign" />
+        <h3 class="card__heading">Your roads and bridges are on the ballot!</h3>
+        <p class="card__text">You can be the vote that finally puts money towards fixing our roads and bridges.</p>
+      </div>
+      <div class="card">
+        <img class="card__image" src="dist/children.jpg" alt="Two young children on a bed." />
+        <h3 class="card__heading">Your childcare & snap benefits are on the ballot!</h3>
+        <p class="card__text">You can be the vote that protects childcare vouchers and SNAP benefits for you and your family.</p>
+      </div>
+    </div>
+
+    <div class="vote">
+      <h3 class="vote__heading">Vote November 5</h2>
+      <p>Voter registration deadline is October 7</p>
+      <p>You can vote absentee; ballots are availble at your Circuit Clerk's office from October 6 until the morning of Nov. 2</p>
+    </div>
+
+    <section class="section plan">
+      <h2 class="offset-heading">Your support matters</h2>
+      <h3 class="section__heading">Make Your Voting Plan</h3>
+      <a class="button--wider section__button" href="https://msvotes.org/voter-services/political-education/" target="_blank" rel="noopener nofollow">Click here to pledge your vote</a>
+      <div class="section__icon-wrapper">
+        <img class="section__icon car" src="dist/car.svg" alt="Car" />
+      </div>
+    </section>
+
+    <section class="section--reverse bank">
+      <h3 class="section__heading">Help Us Phone Bank</h3>
+      <a class="button section__button" href="http://onevoicems.org/election-protection-2019-general-volunteer-application/" target="_blank" rel="nooper nofollow">Click here to volunteer</a>
+      <div class="section__icon-wrapper">
+        <img class="section__icon phone" src="dist/phone-call.svg" alt="Phone" />
+      </div>
+    </section>
+
+    <section class="section polling">
+      <h3 class="section__heading">Check Your Polling Place</h3>
+      <a class="button section__button" href="https://www.msegov.com/sos/voter_registration/AmIRegistered" target="_blank" rel="noopener nofollow">Click here to check your info</a>
+      <div class="section__icon-wrapper">
+        <img class="section__icon map" src="dist/map.svg" alt="Map" />
+      </div>
+    </section>
+
+    <footer class="footer">
+      <img class="footer__logo" src="dist/your-vote-matters.png" alt="Your vote matters!" />
+    </footer>
+
+    <!-- <script src="https://kit.fontawesome.com/ce59932e45.js"></script> -->
+    <!-- <script src="dist/bundle.js"></script> -->
+  </body>
+</html>
