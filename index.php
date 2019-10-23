41aca71ccbc1a77e21b2333a40082156a828d7b0
Will Hawks <will@creativedistillery.com>
2019-10-23 13:59:44 -0500

    add new gtm

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
 <html lang="en">
   <head>
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-88583268-15"></script>
+    <!-- Google Tag Manager -->
     <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-88583268-15');
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KZ3Q34Z');
     </script>
+    <!-- End Google Tag Manager -->
 
 
     <title>Your Vote Matters! | Vote for Change on November 5th</title>
@@ -29,8 +26,8 @@
   <body>
 
     <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLQQQ87"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZ3Q34Z" 
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> 
     <!-- End Google Tag Manager (noscript) -->
 
     <header class="header">
